fca54cbd2f42f4d1c0e507dbaed1f02e3c80fe9f
Raman Krytski <rkeydevelop@gmail.com>
2022-12-19 06:46:04 +0100

    51 - refactoring: deleted comments, code prepared to create promises

--- src/components/Pages/Community/TopUser/TopUserUI.jsx
+++ src/components/Pages/Community/TopUser/TopUserUI.jsx
@@ -7,62 +7,32 @@ const TopUserUI = (props) => {
     axios
         .get("https://social-network.samuraijs.com/api/1.0/users")
         .then(response => {
-            users = response.data.items
-            // return (
-                //     <div key={el.id} className={styles.user}>
-                //         <div>
-                //             {`Name: ${el.firstName} ${el.secondName}`}
-                //         </div>
-                //         <div>{`id: ${el.id}`}</div>
-                //         <div>
-                //             <button onClick={ () => {props.subscr(el.id)}}>
-                //                 {el.subscribed ? 'unsubscribe' : 'subscribe'}
-                //             </button>
-                //         </div>
-                //     </div>
-                // )
+            users = response.data.items;
+            users = users.map(el => {
+                el.firstName = el.name;
+                el.secondName = "SECOND_NAME";
+                el.subscribed = el.followed;
+                el.location = "LOCATION_ERROR"
+                
+
+                return (
+                    <div key={el.id} className={styles.user}>
+                        <div>
+                            {`Name: ${el.firstName} ${el.secondName}`}
+                        </div>
+                        <div>{`id: ${el.id}`}</div>
+                        <div>
+                            <button onClick={() => {
+                                props.subscr(el.id)
+                            }}>
+                                {el.subscribed ? 'unsubscribe' : 'subscribe'}
+                            </button>
+                        </div>
+                    </div>
+                )
+            })
+            console.log(users)
         });
-    debugger
-    // USERS НЕ УСПЕВАЕТ ЗАПИСАТЬСЯ ИЗ АСИНХРОНКИ
-    // ИСПРАВИТЬ ЭТО
-    users = users.map(el => {
-        el.firstName = el.name;
-        el.secondName = "SECOND_NAME";
-        el.subscribed = el.followed;
-        el.location = "LOCATION_ERROR"
-
-        return (
-            <div key={el.id} className={styles.user}>
-                <div>
-                    {`Name: ${el.firstName} ${el.secondName}`}
-                </div>
-                <div>{`id: ${el.id}`}</div>
-                <div>
-                    <button onClick={ () => {props.subscr(el.id)}}>
-                        {el.subscribed ? 'unsubscribe' : 'subscribe'}
-                    </button>
-                </div>
-            </div>
-            )
-    })
-    return users
-
-    // let users = props.state.communityPage.DATA_Users.map(el => {
-    //     return (
-    //         <div key={el.id} className={styles.user}>
-    //             <div>
-    //                 {`Name: ${el.firstName} ${el.secondName}`}
-    //             </div>
-    //             <div>{`id: ${el.id}`}</div>
-    //             <div>
-    //                 <button onClick={ () => {props.subscr(el.id)}}>
-    //                     {el.subscribed ? 'unsubscribe' : 'subscribe'}
-    //                 </button>
-    //             </div>
-    //         </div>
-    //         )
-    // })
-    // return users
 }
 
 export default TopUserUI
